abc0b230f1fb1588f0d60b0591c9f9e2602a1646
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-05-30 16:37:40 +0300

    feat: button, app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./core/header/header.component";
 import { FooterComponent } from "./core/footer/footer.component";
+import { ButtonComponent } from "./shared/ui/button/button.component";
+import { NgClass, NgFor, NgIf, NgStyle } from "@angular/common";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet, HeaderComponent, FooterComponent],
+	imports: [RouterOutlet, HeaderComponent, FooterComponent, ButtonComponent, NgIf, NgFor, NgStyle, NgClass],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
 })
